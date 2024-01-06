70762669be896cdd82222f8d083dc76f32f585af
RavidMarg <ravid.margalit@post.runi.ac.il>
2024-01-06 16:31:39 +0200

    bugfix

--- LoanCalc.java
+++ LoanCalc.java
@@ -80,7 +80,7 @@ public class LoanCalc {
 	*/
 	private static double endBalance(double loan, double rate, int n, double payment) {
 		// Replace the following statement with your code
-		for( int i = 0 ; i <= n; i++ ) {
+		for( int i = 0 ; i < n; i++ ) {
 			loan = (loan - payment)*(1+ rate/100);
 		}
     	return loan;
